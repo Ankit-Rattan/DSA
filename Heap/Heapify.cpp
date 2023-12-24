052faa7a3614f675901edf35ffd6666cfe1105e1
Ankit Rattan <114024068+Ankit-Rattan@users.noreply.github.com>
2023-12-24 14:15:34 +0530

    Update Heapify.cpp

--- Heap/Heapify.cpp
+++ Heap/Heapify.cpp
@@ -1,7 +1,9 @@
 void heapify(int arr[], int n, int i){
     int largest = i;
     int left = 2* i;
+    // int left = 2*i+1    ==> when starting from zero index
     int right = 2* i + 1;
+    // int right = 2*i+2   ==> when starting from zero index
     if(left < n and arr[largest] < arr[left])
         largest = left;
     if(right < n and arr[largest] < arr[right])
