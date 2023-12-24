2e8a48a31e85a58f7a07a8900bacec8410174593
Ankit <rattanankit2004@gmail.com>
2023-12-25 00:56:16 +0530

    insert

--- Hashmaps/m1.cpp
+++ Hashmaps/m1.cpp
@@ -18,9 +18,27 @@ int main()
     pair<string, int> p1("hellome", 1);
     m.insert(p1);
 
-    // M-3
-    m["helloagain"] = 3
+    // M-3:
+    m["helloagain"] = 3;
 
+
+
+    // Search
+    // M-1: 
+    cout<<m["helloagain"]<<endl;
+    // M-2: 
+    cout<<m.at("helloagain")<<endl;
+
+
+    // to check the presence:
+    cout<< m.count("namaste")<<endl;
+
+    // erase:
+    m.erase("hello");
+
+
+    // size
+    cout<<m.size()<<endl;
     
     return 0;
 }
